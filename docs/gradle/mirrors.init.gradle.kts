52ec6a374628d47f43473a58308f01192e880875
yanwk <yanwenkun@foxmail.com>
2020-02-23 08:40:14 +0800

    更新 Gradle 镜像源配置样例

--- docs/gradle/mirrors.init.gradle.kts
+++ docs/gradle/mirrors.init.gradle.kts
@@ -13,6 +13,7 @@ allprojects {
         // Gradle 不推荐启用本地 Maven 源，更不推荐将其置于第一位
         // 1. 本地 Maven 本质上是 Maven 自己专用的 Cache，Gradle 无法对其溯源（无法得知是否完整、是否被篡改）
         // 2. 因此 Gradle 不会对本地 Maven 进行缓存操作，这将会降低构建速度
+        // 3. 只有在真正需要本地制品仓库（即：发布到本地）的情况下，才启用 mavenLocal()
         // 信息来源： https://docs.gradle.org/current/userguide/declaring_repositories.html#sec:case-for-maven-local
 
     }
