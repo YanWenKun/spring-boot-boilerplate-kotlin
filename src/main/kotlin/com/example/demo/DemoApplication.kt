bb93be55a2c4b7b6a63b84b1430d40daac60ed95
yanwk <yanwenkun@foxmail.com>
2020-02-24 11:04:00 +0800

    添加 log 样例

--- src/main/kotlin/com/example/demo/DemoApplication.kt
+++ src/main/kotlin/com/example/demo/DemoApplication.kt
@@ -8,4 +8,13 @@ class DemoApplication
 
 fun main(args: Array<String>) {
     runApplication<DemoApplication>(*args)
+
+    val log = org.slf4j.LoggerFactory.getLogger(DemoApplication::class.java)
+
+    log.error("显示ERROR级日志")
+    log.warn("显示WARN级日志")
+    log.info("显示INFO级日志")
+    log.debug("显示DEBUG级日志")
+    log.trace("显示TRACE级日志")
+
 }
