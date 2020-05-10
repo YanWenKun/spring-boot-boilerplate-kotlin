a7d1ac7f8827867c3344ad3431f0b8222fbddaa2
YAN Wenkun <code@yanwk.fun>
2020-05-11 05:58:05 +0800

    升级 Gradle Wrapper 到 6.4

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -2,7 +2,7 @@
  * 该 settings 文件用于指定构建（build）时包含哪些项目（projects）
  *
  * 如何使用Gradle配置多工程构建，可参考官方手册：
- * https://docs.gradle.org/6.0.1/userguide/multi_project_builds.html
+ * https://docs.gradle.org/current/userguide/multi_project_builds.html
  */
 
 rootProject.name = "demo"
